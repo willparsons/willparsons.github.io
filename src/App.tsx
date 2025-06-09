ad59c1f8af959855bb549a75fe8a63d5bf5f06dc
William Parsons-Heins <williamparsons62@gmail.com>
2025-06-09 20:32:13 +0100

    UI update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import type { Component } from 'solid-js';
-
-const App: Component = () => {
+export function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <div class="h-full container m-auto text-gray-50">
+      <div class="w-full h-full px-24 flex items-center">
+        <div class="w-1/2">
+          <div class="flex gap-4">
+            <span class="badge badge-ghost">JS/TS</span>
+            <span class="badge badge-ghost">Python</span>
+            <span class="badge badge-ghost">React</span>
+          </div>
+
+          <h1 class="text-8xl">Hi, I'm<br /><span class="text-indigo-400">William</span>.</h1>
+
+          <p class="text-lg mt-4 mb-4">An experienced full-stack website developer with a passion for crafting unique digital experiences.</p>
+
+          <div class="flex flex-row gap-4">
+            <button class="btn w-32 btn-primary">Get In Contact</button>
+            <button class="btn w-32 btn-outline btn-secondary">Projects</button>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <img src="https://www.freevector.com/uploads/vector/preview/4791/FreeVector-Geometric-Vector-Pattern.jpg" class="w-full rounded-2xl border-neutral-800 border-2" />
+        </div>
+      </div>
+    </div>
   );
 };
-
-export default App;
