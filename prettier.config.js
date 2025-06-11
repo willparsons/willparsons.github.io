7de9376711fbb31cf4f370cc51330476c79e1ee7
William Parsons-Heins <williamparsons62@gmail.com>
2025-06-11 21:57:40 +0100

    setup prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+    printWidth: 120,
+    useTabs: false,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solidPlugin(), tailwindcss()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+    plugins: [solidPlugin(), tailwindcss()],
+    server: {
+        port: 3000,
+    },
+    build: {
+        target: "esnext",
+    },
 });
