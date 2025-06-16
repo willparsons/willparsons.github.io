e12537cb4b37ae3be412751a24b4925a466398c2
William Parsons-Heins <williamparsons62@gmail.com>
2025-06-16 18:49:18 +0100

    change the project image to a skeleton for now

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,6 +1,6 @@
 export function Project(props: { name: string; image: string }) {
     return (
-        <div class={`flex items-end rounded-2xl bg-[url(${props.image})] w-96 h-96 bg-cover flex-grow`}>
+        <div class="h-96 flex items-end skeleton flex-grow">
             <div class="w-full px-8 py-4 bg-black/50 backdrop-blur-xl rounded-b-2xl">
                 <p>{props.name}</p>
             </div>
