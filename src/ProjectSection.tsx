0fb82affcd1feeff6766e68cdca8469b90b82e60
William Parsons-Heins <williamparsons62@gmail.com>
2025-06-16 17:49:37 +0100

    add subtitle to the 'My Projects' header

--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -3,7 +3,11 @@ import { Project } from "./Project";
 export function ProjectSection() {
     return (
         <section id="projects" class="w-full min-h-screen flex flex-col justify-center lg:px-32">
-            <h1 class="text-6xl mb-8">My Projects</h1>
+            <h1 class="text-6xl mb-4">My Projects</h1>
+            <p class="mb-8 text-lg text-gray-400">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tellus velit, lacinia eleifend
+                pharetra sit amet, facilisis ac nisl. Phasellus.{" "}
+            </p>
             <div class="flex gap-4 justify-between">
                 <Project
                     name="Some project"
