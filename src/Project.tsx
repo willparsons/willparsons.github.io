dfe19ddaa6623b1fb9978e543b250dad5d35d771
William Parsons-Heins <williamparsons62@gmail.com>
2025-06-23 17:26:47 +0100

    update the project section

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,8 +1,41 @@
-export function Project(props: { name: string; image: string }) {
+import { AiOutlineStar } from "solid-icons/ai";
+import { For, JSXElement } from "solid-js";
+
+export type ProjectProps = {
+    name: string;
+    description: string;
+    image: string;
+    learnings: string[];
+    technologies: JSXElement[];
+};
+
+export function Project(props: ProjectProps) {
     return (
-        <div class="h-96 flex items-end skeleton flex-grow">
-            <div class="w-full px-8 py-4 bg-black/50 backdrop-blur-xl rounded-b-2xl">
-                <p>{props.name}</p>
+        <div class="flex flex-row gap-4">
+            <div class="h-96 flex items-end skeleton flex-grow">
+                <div class="w-full px-8 py-4 bg-black/50 backdrop-blur-xl rounded-b-2xl">
+                    <p>{props.name}</p>
+                </div>
+            </div>
+
+            <div class="w-96">
+                <h1 class="text-4xl font-bold">{props.name}</h1>
+
+                <p class="text-gray-400">{props.description}</p>
+
+                <ul class="list-inside list-none my-8">
+                    <For each={props.learnings}>
+                        {(item) => (
+                            <li class="flex items-center gap-2">
+                                <AiOutlineStar size={20} class="text-purple-300" /> <span>{item}</span>
+                            </li>
+                        )}
+                    </For>
+                </ul>
+
+                <div class="flex flex-wrap gap-2">
+                    <For each={props.technologies}>{(item) => <div class="badge badge-xl">{item}</div>}</For>
+                </div>
             </div>
         </div>
     );
--- src/ProjectSection.tsx
+++ src/ProjectSection.tsx
@@ -1,21 +1,67 @@
+import { SiDaisyui, SiNextdotjs } from "solid-icons/si";
 import { Project } from "./Project";
+import { FaBrandsReact } from "solid-icons/fa";
+import { BiLogosFirebase, BiLogosTailwindCss, BiLogosTypescript } from "solid-icons/bi";
+import { TbBrandSolidjs } from "solid-icons/tb";
 
 export function ProjectSection() {
     return (
-        <section id="projects" class="w-full min-h-screen flex flex-col justify-center lg:px-32">
+        <section id="projects" class="w-full min-h-screen flex flex-col justify-center lg:px-64">
             <h1 class="text-6xl mb-4">My Projects</h1>
-            <p class="mb-8 text-lg text-gray-400">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tellus velit, lacinia eleifend
-                pharetra sit amet, facilisis ac nisl. Phasellus.{" "}
-            </p>
-            <div class="flex gap-4 justify-between">
+            <p class="mb-8 text-lg text-gray-400">A highlight of some of the things I've worked on lately.</p>
+            <div class="flex flex-col gap-4 justify-between">
                 <Project
-                    name="Some project"
+                    name="Prompto"
+                    description="A worlde inspired guessing game where users try to figure out the prompt that generated the image."
+                    learnings={["Foo", "Bar", "Baz"]}
                     image="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    technologies={[
+                        <>
+                            <SiNextdotjs class="text-white" />
+                            Next.js
+                        </>,
+
+                        <>
+                            <FaBrandsReact class="text-cyan-400" />
+                            React
+                        </>,
+                        <>
+                            <BiLogosTailwindCss class="text-cyan-400" />
+                            Tailwind CSS
+                        </>,
+                        <>
+                            <BiLogosFirebase class="text-orange-400" />
+                            Firebase
+                        </>,
+                        <>
+                            <BiLogosTypescript class="text-blue-400" />
+                            TypeScript
+                        </>,
+                    ]}
                 />
                 <Project
-                    name="Another project"
+                    name="My Portfolio"
+                    description="The site you're on right now!"
+                    learnings={["Foo", "Bar", "Baz"]}
                     image="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    technologies={[
+                        <>
+                            <TbBrandSolidjs class="text-blue-400" />
+                            Solid.js
+                        </>,
+                        <>
+                            <BiLogosTailwindCss class="text-cyan-400" />
+                            Tailwind CSS
+                        </>,
+                        <>
+                            <BiLogosTypescript class="text-blue-400" />
+                            TypeScript
+                        </>,
+
+                        <>
+                            <SiDaisyui size={70} />
+                        </>,
+                    ]}
                 />
             </div>
         </section>
