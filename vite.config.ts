7fb76ab7bae73cc99b20ed8520ef3f370bc179d7
William Parsons-Heins <williamparsons62@gmail.com>
2025-07-04 20:03:08 +0100

    add vite-plugin-solid-svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import solidSvg from "vite-plugin-solid-svg";
 
 export default defineConfig({
-    plugins: [solidPlugin(), tailwindcss()],
+    plugins: [solidPlugin(), tailwindcss(), solidSvg()],
     server: {
         port: 3000,
     },
